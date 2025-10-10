a0bf1091dc39c6e5e3c5bf0ed835c1927429ea07
Martin Rumori <martin.rumori@sonible.com>
2025-10-10 21:10:30 +0200

    AppControlComponent: Add simple GUI component to control apps (IMRV-18).
    MainComponent: Dynamically add app controls if notified by AppController.

---
+++ src/view/AppControlComponent.cpp
@@ -0,0 +1,99 @@
+/*
+    AppControlComponent.cpp
+
+    Part of MR Lab Control Software
+
+    SPDX-License-Identifier: EUPL-1.2
+    SPDX-FileCopyrightText: Copyright (c) 2025 Martin Rumori/sonible GmbH
+ */
+
+#include "AppControlComponent.h"
+
+
+namespace mrlab::view
+{
+
+AppControlComponent::AppControlComponent (controller::AppHandle& appHandle)
+    : app (appHandle),
+      appIdLabel ("AppControlComponent::appIdLabel", app.getConfig().id.toString()),
+      statusLabel ("AppControlComponent::statusLabel"),
+      launchQuitButton ("AppControlComponent::launchQuitButton"),
+      killButton ("AppControlComponent::killButton"),
+      openOutputButton ("AppControlComponent::openOutputButton")
+{
+    killButton.setButtonText ("kill");
+    openOutputButton.setButtonText ("output");
+
+    launchQuitButton.setClickingTogglesState (true);
+    openOutputButton.setEnabled (false);
+
+    launchQuitButton.onClick = [&] {
+        launchQuitButton.getToggleState() ? app.start() : app.stop();
+    };
+
+    killButton.onClick = [&] { app.kill(); };
+
+    // TODO: Implement app output to logging (IMRV-38).
+    // Just post the app output to stdout on button click for now.
+    openOutputButton.onClick = [&] {
+        std::cout << app.getOutput().toStdString();
+        openOutputButton.setEnabled (false);
+    };
+
+    addAndMakeVisible (appIdLabel);
+    addAndMakeVisible (statusLabel);
+    addAndMakeVisible (launchQuitButton);
+    addAndMakeVisible (killButton);
+    addAndMakeVisible (openOutputButton);
+
+    app.addListener (this);
+    appStateChanged (app, app.getState());
+}
+
+AppControlComponent::~AppControlComponent()
+{
+    app.removeListener (this);
+}
+
+void AppControlComponent::appStateChanged (controller::AppHandle&, controller::AppHandle::AppState newState)
+{
+    launchQuitButton.setToggleState (app.isRunning(), juce::dontSendNotification);
+    launchQuitButton.setButtonText (app.isRunning() ? "quit" : "launch");
+    launchQuitButton.setEnabled (newState != controller::AppHandle::AppState::alive);
+
+    killButton.setEnabled (app.isRunning());
+
+    auto statusText = controller::AppHandle::appStateNames.at (newState);
+
+    if (app.isFinished())
+        statusText += " (" + juce::String (app.getExitCode()) + ")";
+
+    statusLabel.setText (statusText, juce::dontSendNotification);
+}
+
+void AppControlComponent::appOutputAvailable (controller::AppHandle&, const juce::String&)
+{
+    openOutputButton.setEnabled (true);
+
+    // TODO: Implement app output to logging (IMRV-38).
+}
+
+void AppControlComponent::paint (juce::Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+}
+
+void AppControlComponent::resized()
+{
+    const auto h = getHeight();
+    const auto bw = 3 * h;
+    const auto lw = juce::jmax (2 * h, (getWidth() - 3 * bw) / 2);
+
+    appIdLabel.setBounds (0, 0, lw, h);
+    launchQuitButton.setBounds (appIdLabel.getRight(), 0, bw, h);
+    statusLabel.setBounds (launchQuitButton.getRight(), 0, lw, h);
+    killButton.setBounds (statusLabel.getRight(), 0, bw, h);
+    openOutputButton.setBounds (killButton.getRight(), 0, bw, h);
+}
+
+} // namespace mrlab::view
--- src/view/MainComponent.cpp
+++ src/view/MainComponent.cpp
@@ -8,7 +8,11 @@
  */
 
 #include "MainComponent.h"
+#include "AppControlComponent.h"
 #include <controller/MainController.h>
+#include <controller/AppHandle.h>
+#include <controller/AppConfigController.h>
+
 
 namespace mrlab::view
 {
@@ -16,7 +20,14 @@ namespace mrlab::view
 MainComponent::MainComponent (controller::MainController& controller)
     : mainController (controller)
 {
-    setSize (600, 400);
+    mainController.getAppController().addListener (this);
+
+    setSize (800, 300);
+}
+
+MainComponent::~MainComponent()
+{
+    mainController.getAppController().removeListener (this);
 }
 
 void MainComponent::paint (juce::Graphics& g)
@@ -25,6 +36,30 @@ void MainComponent::paint (juce::Graphics& g)
 }
 
 void MainComponent::resized()
-{}
+{
+    auto y = 0;
+
+    for (auto& control : appControls)
+        control->setBounds (50, y += 50, getWidth() - 100, 35);
+}
+
+void MainComponent::appAdded (controller::AppHandle& app)
+{
+    auto& c = appControls.emplace_back (std::make_unique<AppControlComponent> (app));
+    addAndMakeVisible (*c);
+    resized();
+}
+
+void MainComponent::appWillBeRemoved (controller::AppHandle& app)
+{
+    const auto it = std::find_if (appControls.begin(), appControls.end(), [id = app.getConfig().id] (auto& c) { return c->getId() == id; });
+
+    if (it == appControls.end())
+        return;
+
+    removeChildComponent (it->get());
+    appControls.erase (it);
+    resized();
+}
 
 } // namespace mrlab::view
