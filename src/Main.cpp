70effa58ae7f7c2ef990fc13ea002ded1b18c77b
Martin Rumori <martin.rumori@sonible.com>
2025-10-28 08:55:01 +0100

    Main.cpp: On exit, warn if apps are still running and offer to stop them.

--- src/Main.cpp
+++ src/Main.cpp
@@ -38,7 +38,34 @@ public:
         mainController.reset();
     }
 
-    void systemRequestedQuit() override { quit(); }
+    void systemRequestedQuit() override
+    {
+        if (mainController->getAppController().isAnyAppRunning())
+        {
+            auto callWhenAppsStopped = [&] (bool timedOut) {
+
+                if (timedOut)
+                    mainController->getAppController().killAllApps();
+
+                quit();
+            };
+
+            auto messageBoxCallback = [&, whenStopped = std::move (callWhenAppsStopped)] (bool result) {
+                if (result)
+                    mainController->getAppController().stopAllApps (std::move (whenStopped), 2000);
+            };
+
+            juce::NativeMessageBox::showOkCancelBox (juce::MessageBoxIconType::WarningIcon,
+                                                     "Apps are running",
+                                                     "There are still managed apps running. "
+                                                     "Ok to quit all apps?",
+                                                     mainWindow.get(),
+                                                     juce::ModalCallbackFunction::create (std::move (messageBoxCallback)));
+            return;
+        }
+
+        quit();
+    }
 
     void anotherInstanceStarted (const juce::String& /*commandLine*/) override {}
 
