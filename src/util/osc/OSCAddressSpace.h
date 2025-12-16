54a6be938202cfd1b767ef925a26464359022531
Martin Rumori <martin.rumori@sonible.com>
2025-12-16 10:31:18 +0100

    OSCAddressSpace: Initial sketch of OSC address tree for dispatching.

---
+++ src/util/osc/OSCAddressSpace.h
@@ -0,0 +1,36 @@
+/*
+    OSCAddressSpace.h
+
+    Part of MR Lab Control Software
+
+    SPDX-License-Identifier: EUPL-1.2
+    SPDX-FileCopyrightText: Copyright (c) 2025 Martin Rumori/sonible GmbH
+ */
+
+#pragma once
+
+namespace mrlab::osc
+{
+
+//==============================================================================
+/** Dispatch OSC packets to listeners to OSC addresses or address subtrees.
+ */
+class OSCAddressSpace
+{
+public:
+    //==============================================================================
+    /**
+
+     */
+    OSCAddressSpace();
+
+
+private:
+    //==============================================================================
+
+    //==============================================================================
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (OSCAddressSpace)
+};
+
+} // namespace mrlab::osc
