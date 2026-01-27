58913efd08285b6af9481138e00878f24ae45b78
Piotr Majdak <piotr.majdak@oeaw.ac.at>
2026-01-27 14:17:02 +0100

    Demo Jungle added

---
+++ resources/WebGUI/scripts/demo_jungle.js
@@ -0,0 +1,63 @@
+export function init()
+{
+	document.getElementById("pd_jungle-status").innerText = "initialized";
+	document.getElementById("pd_jungle-status").style.backgroundColor = "";	
+	document.getElementById("pd_jungle-Control_Version").innerText = "---";
+	document.getElementById("pd_jungle-Control_SampleRate").innerText = "---";
+	document.getElementById("pd_jungle-Control_CPULoad").innerText = "---";
+	var level = document.getElementById("pd_jungle-Level");
+	console.log(level);
+	document.getElementById("pd_jungle-Level").addEventListener(
+		"change",
+		(e) => {
+			console.log("Level: change registered");
+		},
+		false,
+	);			
+	console.log("Level on change listener added");
+	console.log(level);
+}
+
+export function connect()
+{
+	state = document.getElementById("pd_jungle-status");
+	state.innerText = "connecting...";
+	state.style.backgroundColor = "blue";
+	sendResponse('/app/pd_jungle/osc/Control/Response');
+	sendNoArgs('/app/pd_jungle/osc/Control/Version');
+	secWaited = 0;
+	setTimeout(() => { checkConnection(); }, 100);
+}
+
+export function checkConnection()
+{
+	const timeout = 1000; // timeout in ms
+	secWaited += 100;
+	state = document.getElementById("pd_jungle-Control_Version");
+	console.log("waiting for " + secWaited/1000 + "s, version=" + state.innerText);
+	switch (state.innerText)
+	{
+		case "---":
+			if (secWaited <= timeout)
+			{		// try again
+				sendResponse('/app/pd_jungle/osc/Control/Response', 9340);
+				sendNoArgs('/app/pd_jungle/osc/Control/Version');		
+				setTimeout(() => { checkConnection(); }, 100);
+			}
+			break;
+		default: // done!
+			state = document.getElementById("pd_jungle-status");
+			state.innerText = "connected";
+			state.style.backgroundColor = "";
+			sendNoArgs('/app/pd_jungle/osc/Control/CPULoad');		
+			sendNoArgs('/app/pd_jungle/osc/Control/SampleRate');		
+			break;
+	}
+	if (secWaited > timeout)
+	{ 	// time out -> clean up
+		state = document.getElementById("pd_jungle-status");
+		state.innerText = "time out while connecting";
+		state.style.backgroundColor = "red";
+	}
+}		
+
