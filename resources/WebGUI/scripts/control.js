d32c666851f7f47eee1837604167bef0f5127df9
Michael Mihocic <michael.mihocic@oeaw.ac.at>
2026-01-27 17:12:27 +0100

    WebGUI: Get Amps status, first attempts working, just for testing

--- resources/WebGUI/scripts/control.js
+++ resources/WebGUI/scripts/control.js
@@ -2,3 +2,29 @@ export function init()
 {
 }
 
+const btn = document.querySelector('#control-Update_Status');
+const display = document.querySelector('#amp1-status');
+
+btn.addEventListener('click', async () => 
+{
+  const url = 'http://172.16.60.112/api/get/general/model';
+  
+ try 
+  {
+    const response = await fetch(url, { signal: AbortSignal.timeout(100) } );
+    if (!response.ok) 
+	{
+      throw new Error(`Error: ${response.status}`);
+    }
+    display.textContent = await response.text();
+  } 
+  catch (error) 
+  {
+	if (error.name === 'TimeoutError') 
+	{ 
+	  display.textContent = `D`; 
+	 }
+    else	 
+      display.textContent = `Error: ${error.message}`;
+  }
+});
