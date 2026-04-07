801aaf6d94c02b434de28337338c025120f53585
Piotr Majdak <piotr.majdak@oeaw.ac.at>
2026-04-07 14:15:50 +0200

    demo_fly: UI more robust

--- resources/WebGUI/scenes/demo_fly.js
+++ resources/WebGUI/scenes/demo_fly.js
@@ -4,6 +4,9 @@ export function init()
 	document.getElementById("pd_fly-status").style.backgroundColor = "";	
 	document.getElementById("pd_fly-Control_Version").innerText = "---";
 	document.getElementById("pd_fly-Control_SampleRate").innerText = "---";
+  document.getElementById("pd_fly-Play_Start").disabled = true;
+  document.getElementById("pd_fly-Play_Stop").disabled = true;
+  document.getElementById("volume-slider").disabled = true;
 
   document.getElementById('pd_fly-Play_VU').addEventListener('updated', (e) => 
   {
@@ -68,6 +71,9 @@ export function checkConnection()
 			sendNoArgs('/app/pd_fly/osc/Control/SampleRate');
 			sendNoArgs('/app/pd_fly/osc/Play/Volume');
 			sendNoArgs('/app/pd_fly/osc/Play/Duration');
+      document.getElementById("pd_fly-Play_Start").disabled = false;
+      document.getElementById("pd_fly-Play_Stop").disabled = false;
+      document.getElementById("volume-slider").disabled = false;
 			break;
 	}
 	if (secWaited > timeout)
